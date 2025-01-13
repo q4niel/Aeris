acc1a48d2242918c764c24967aa77a41336d5bd4
q4niel <120566282+q4niel@users.noreply.github.com>
2025-01-13 17:24:03 +0100

    [logging] std::cout wrappers (#3)
    - **write**: Prints all arguments after one another
    - **print**: Prints all arguments after one another, and a newline at the end.
    - **trace**: Prints all arguments on separate lines.
    - **newline**: Prints the amount of newlines based on the given argument.

---
+++ src/io.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifdef DEBUG
+#define NO_IMPL ;
+#else
+#define NO_IMPL {}
+#endif
+
+namespace io {
+    template<typename T, typename ...Args>
+    void write(T t, Args ...args)NO_IMPL
+
+    template<typename ...Args>
+    void print(Args ...args)NO_IMPL
+
+    template<typename ...Args>
+    void trace(Args ...args)NO_IMPL
+
+    void newline(unsigned int count)NO_IMPL
+}
+
+#ifdef DEBUG
+#include "io.t.hpp"
+#endif
