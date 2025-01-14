7341228c7cc0b473f2b8a65f6a51999550813047
q4niel <120566282+q4niel@users.noreply.github.com>
2025-01-14 18:46:13 +0100

    Revert "[logging-rework] New function names, and namespace" (#6)
    This reverts commit 4dc246258da1a47b004480b8bb4db712f297d152.

--- src/io.t.hpp
+++ src/io.t.hpp
@@ -1,30 +1,30 @@
 #pragma once
-#include "print.hpp"
+#include "io.hpp"
 #include <iostream>
 
 template<typename T, typename ...Args>
-void ars__::print__(T t, Args ...args) {
+void ars::write(T t, Args ...args) {
     std::cout << t;
     if constexpr (sizeof ...(Args) > 0) {
-        print__(args...);
+        write(args...);
     }
 }
 
 template<typename ...Args>
-void ars__::println__(Args ...args) {
-    print__(args...);
-    printsep__(1);
+void ars::print(Args ...args) {
+    write(args...);
+    newline(1);
 }
 
 template<typename ...Args>
-void ars__::printlns__(Args ...args) {
+void ars::trace(Args ...args) {
     for (const auto &arg : {args...}) {
-        print__(arg);
-        printsep__(1);
+        write(arg);
+        newline(1);
     }
 }
 
-void ars__::printsep__(unsigned int count = 1) {
+void ars::newline(unsigned int count = 1) {
     while (count --> 0) {
         std::cout << '\n';
     }
